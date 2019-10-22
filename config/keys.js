a472723005e075c6a0e46dfec771e52663b364b3
Deepali1212 <deepalikatyal12@gmail.com>
2019-10-22 12:45:29 +0530

    profile dashboard

--- config/keys.js
+++ config/keys.js
@@ -1,4 +1,5 @@
 module.exports = {
 mongoURI:
-"mongodb+srv://deepali:deepali@cluster0-y33hs.mongodb.net/test?retryWrites=true&w=majority"
+"mongodb+srv://deepali:deepali@cluster0-y33hs.mongodb.net/test?retryWrites=true&w=majority",
+secretOrKey: 'secret'
 };
--- server.js
+++ server.js
@@ -1,10 +1,15 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
 const users = require('./routes/api/users');
 const profile = require('./routes/api/profile');
 const posts = require('./routes/api/posts');
+const passport = require('passport');
 
 const app = express();
+//Body parser middleware
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.json());
 // DB Config
 const db = require('./config/keys').mongoURI;
 //Connect to mongodb
@@ -13,6 +18,10 @@ mongoose
     .then(() => console.log('MongoDB Connected'))
     .catch(err => console.log(err));
 app.get('/',(req,res) => res.send("Hello !"));
+//passport middleware
+app.use(passport.initialize());
+//passport config
+require('./config/passport')(passport);
 
 //routes
 app.use('/api/users',users);
